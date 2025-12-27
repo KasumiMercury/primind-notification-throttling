0d5a2e96a452047397acfffe206b952fce935050
KasumiMercury <naoki_shira@outlook.jp>
2025-12-27 21:30:26 +0900

    chore: add tracing propagation utils

---
+++ internal/observability/tracing/propagation.go
@@ -0,0 +1,22 @@
+package tracing
+
+import (
+	"context"
+	"net/http"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+func ExtractFromHTTPRequest(ctx context.Context, r *http.Request) context.Context {
+	return otel.GetTextMapPropagator().Extract(ctx, propagation.HeaderCarrier(r.Header))
+}
+
+func InjectToHTTPRequest(ctx context.Context, r *http.Request) {
+	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(r.Header))
+}
+
+// for message attributes
+func InjectToMap(ctx context.Context, carrier map[string]string) {
+	otel.GetTextMapPropagator().Inject(ctx, propagation.MapCarrier(carrier))
+}
