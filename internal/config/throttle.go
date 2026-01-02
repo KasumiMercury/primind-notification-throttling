2e09dc38de8915d1bdec13b7b65301073d75b2fb
KasumiMercury <naoki_shira@outlook.jp>
2026-01-02 22:48:22 +0900

    refactor: remove unused tolerance config

--- internal/config/throttle.go
+++ internal/config/throttle.go
@@ -9,19 +9,16 @@ const (
 	requestCapPerMinuteEnv   = "REQUEST_CAP_PER_MINUTE"
 	confirmWindowMinutesEnv  = "CONFIRM_WINDOW_MINUTES"
 	planningWindowMinutesEnv = "PLANNING_WINDOW_MINUTES"
-	toleranceEnv             = "TOLERANCE"
 
 	defaultRequestCapPerMinute   = 60
 	defaultConfirmWindowMinutes  = 5
 	defaultPlanningWindowMinutes = 30
-	defaultTolerance             = 0.2 // 20% tolerance
 )
 
 type ThrottleConfig struct {
 	RequestCapPerMinute   int
 	ConfirmWindowMinutes  int
 	PlanningWindowMinutes int
-	Tolerance             float64 // Tolerance for ramp-up deviation (0.0 - 1.0)
 }
 
 func LoadThrottleConfig() *ThrottleConfig {
@@ -46,17 +43,9 @@ func LoadThrottleConfig() *ThrottleConfig {
 		}
 	}
 
-	tolerance := defaultTolerance
-	if v := os.Getenv(toleranceEnv); v != "" {
-		if parsed, err := strconv.ParseFloat(v, 64); err == nil && parsed >= 0.0 && parsed <= 1.0 {
-			tolerance = parsed
-		}
-	}
-
 	return &ThrottleConfig{
 		RequestCapPerMinute:   requestCap,
 		ConfirmWindowMinutes:  confirmWindow,
 		PlanningWindowMinutes: planningWindow,
-		Tolerance:             tolerance,
 	}
 }
